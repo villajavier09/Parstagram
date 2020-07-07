cd47b0c13fe5502ffc083ad8ee5bc9b873178076
Javier Villalpando <villajavier@fb.com>
2020-07-07 17:36:12 -0400

    Added ability to create a post and take a picture for the post using camera

--- app/src/main/java/com/javiervillalpando/parstagram/LoginActivity.java
+++ app/src/main/java/com/javiervillalpando/parstagram/LoginActivity.java
@@ -8,25 +8,33 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String TAG = "LoginActivity";
     private EditText usernameLogin;
     private EditText passwordLogin;
     private Button loginButton;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+        if(ParseUser.getCurrentUser() != null){
+            goToMainActivity();
+        }
+
         usernameLogin = findViewById(R.id.usernameLogin);
         passwordLogin = findViewById(R.id.passwordLogin);
         loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +42,15 @@ public class LoginActivity extends AppCompatActivity {
                 String username = usernameLogin.getText().toString();
                 String password = passwordLogin.getText().toString();
                 loginUser(username,password);
+            }
+        });
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"onClick signup button");
+                String username = usernameLogin.getText().toString();
+                String password = passwordLogin.getText().toString();
+                signUpUser(username,password);
             }
         });
     }
@@ -44,17 +61,35 @@ public class LoginActivity extends AppCompatActivity {
             public void done(ParseUser user, ParseException e) {
                 if(e !=null){
                     Log.e(TAG,"login issue",e);
+                    Toast.makeText(LoginActivity.this,"Incorrect username or password",Toast.LENGTH_SHORT).show();
                     return;
                 }
                 //If login successful, go to main activity
                 goToMainActivity();
+                Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
             }
 
-            private void goToMainActivity() {
-                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
+
+        });
+    }
+    private void signUpUser(final String username, final String password){
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null){
+                    loginUser(username,password);
+                } else{
+                    Toast.makeText(LoginActivity.this,"Username or password already taken",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+    private void goToMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
